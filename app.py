f21fe6e444f80ce0f48bafff2a09121d54622028
tt1125 <tsurutsuru275@gmail.com>
2025-05-28 04:16:15 +0900

    feat: 0.0.0.0にバインドする設定を追記

--- app.py
+++ app.py
@@ -20,5 +20,8 @@ def image():
     return send_file("image.png", mimetype="image/png")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # RenderからPORTが来なければ5000で起動
+    app.run(debug=False, host="0.0.0.0", port=port)
